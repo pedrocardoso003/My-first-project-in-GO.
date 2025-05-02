01edeeda6c86076cce34437fdb037cc353fe79e1
Pedro <pedro_ca_03@outlook.com>
2025-05-02 01:21:03 +0100

    board initialization implemented

--- main.go
+++ main.go
@@ -2,14 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
-type board [4][4]int
+//TODO: Implementar logica de movimento
+/*
+	numeros para uma direcao
+	se dois numeros iguais coliderem eles somam-se
+	apos cada movimento adicionar um novo random tile
+*/
+
+var board [4][4]int
 
 func main() {
 	fmt.Println("Hello, World!")
+	initializeBoard()
+	fmt.Println("Initial Board:")
+	fmt.Println(board)
 }
 
-func createBoard() board {
-	return board{}
+func initializeBoard() {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			board[i][j] = 0
+		}
+	}
+	for i := 0; i < 2; i++ {
+		addRandomTile()
+	}
+}
+
+func addRandomTile() {
+	//rand.intn(n) returns a random number between 0 and n
+
+	randomNumber := rand.Intn(2) //only 2 or 4
+
+	if randomNumber == 0 {
+		randomNumber = 2
+	} else {
+		randomNumber = 4
+	}
+
+	//make sure the random number is added to an empty cell
+	for {
+		if board[rand.Intn(4)][rand.Intn(4)] == 0 {
+			board[rand.Intn(4)][rand.Intn(4)] = randomNumber
+			break
+		}
+	}
 }
